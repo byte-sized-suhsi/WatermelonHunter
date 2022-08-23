6fc25bc1579c1a62d6dd07b01e6115d692ca4ee6
byte-sized-sushi <bytesizedsushi@gmail.com>
2022-08-23 12:00:57 +0200

    .

--- src/main/java/org/example/Player.java
+++ src/main/java/org/example/Player.java
@@ -1,9 +1,25 @@
 package org.example;
 
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
+
+import java.io.IOException;
+
 public class Player extends Movable
 {
     @Override
-    public void move() {
+    public void move() throws IOException, InterruptedException {
         // TODO: förflytta spelaren
+        KeyStroke keyStroke = Main.readUserInputType();
+        KeyType type = keyStroke.getKeyType();
+        String typeString = type.name();
+
+        /*
+        switch(type)
+        {
+            case "ArrowUp":
+                break;
+
+        }*/
     }
 }
